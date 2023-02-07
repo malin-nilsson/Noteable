b9e209801f4b03d67d26361e5ba4f7b2251c5d18
Malin Nilsson <malinnilsson87@gmail.com>
2023-02-07 19:12:30 +0100

    Add Note form styling

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -2,7 +2,25 @@
 </script>
 
 <template>
+
+ 
 <div class="notes">
+  <div class="card has-background-link-light p-4 mb-5"> 
+    <div class="field">
+  <div class="control">
+    <textarea class="textarea" placeholder="Add a new note"></textarea>
+  </div>
+</div>
+
+<div class="field is-grouped is-grouped-right">
+  <div class="control">
+    <button class="button is-link has-background-link-dark">
+      Add New Note
+    </button>
+  </div>
+</div>
+
+</div>
     <div class="card mb-4">
   <div class="card-content">
     <div class="content">
