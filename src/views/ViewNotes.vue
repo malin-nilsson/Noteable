e23795754110fa3d8164b6bfa2c3b05682dbe184
Malin Nilsson <malinnilsson87@gmail.com>
2023-02-07 19:05:59 +0100

    Notes styling

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,6 +3,16 @@
 
 <template>
 <div class="notes">
-    Notes
+    <div class="card mb-4">
+  <div class="card-content">
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">Edit</a>
+    <a href="#" class="card-footer-item">Delete</a>
+  </footer>
+</div>
 </div>
 </template>
