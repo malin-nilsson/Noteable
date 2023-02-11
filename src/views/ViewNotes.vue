37275f5e6efe5f194def1cf104ce30ec37cc4ea9
Malin Nilsson <malinnilsson87@gmail.com>
2023-02-11 10:37:18 +0100

    Add Note method

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,4 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+/* 
+notes
+*/
+
+const newNote = ref('')
+const notes = ref([
+  {
+    id: 'id1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.'
+  },
+  {
+    id: 'id2',
+    content: 'This is a short note - woho'
+  },
+])
+const newNoteRef = ref<HTMLTextAreaElement>()
+
+const addNote = () => {
+  let currentDate = new Date().getTime()
+  let id = currentDate.toString()
+
+  let note = {
+    id: id,
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+
+  newNote.value = ''
+
+if (newNoteRef.value) newNoteRef.value.focus()
+}
+
+
 </script>
 
 <template>
@@ -8,23 +44,33 @@
   <div class="card has-background-link-light p-4 mb-5"> 
     <div class="field">
   <div class="control">
-    <textarea class="textarea" placeholder="Add a new note"></textarea>
+    <textarea 
+    v-model="newNote"
+    class="textarea" 
+    placeholder="Add a new note"
+    ref="newNoteRef"></textarea>
   </div>
 </div>
 
 <div class="field is-grouped is-grouped-right">
   <div class="control">
-    <button class="button is-link has-background-link-dark">
+    <button 
+    @click="addNote"
+    :disabled="!newNote"
+    class="button is-link has-background-link-dark">
       Add New Note
     </button>
   </div>
 </div>
-
 </div>
-    <div class="card mb-4">
+
+    <div 
+    v-for="note in notes"
+    :key="note.id"
+    class="card mb-4">
   <div class="card-content">
     <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+     {{note.content}}
     </div>
   </div>
   <footer class="card-footer">
