1884b50628428859769777768fd0109391403e0e
Malin Nilsson <malinnilsson87@gmail.com>
2023-03-04 11:47:23 +0100

    Delete modal (keyboard control)

--- src/components/Notes/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote.vue
@@ -1,58 +1,73 @@
-<script lang="ts" setup>
-import { onClickOutside } from '@vueuse/core'
-import { ref } from 'vue';
+  <script lang="ts" setup>
+  import { onClickOutside } from '@vueuse/core'
+  import { onMounted, onUnmounted, ref } from 'vue';
 
-/*
-props
-*/
-const props = defineProps({
-modelValue: {
-    type: Boolean,
-    default: false
-}
-})
+  /*
+  props
+  */
+  const props = defineProps({
+  modelValue: {
+      type: Boolean,
+      default: false
+  }
+  })
 
-/*
-emits
-*/
-const emit = defineEmits(['update:modelValue'])
+  /*
+  emits
+  */
+  const emit = defineEmits(['update:modelValue'])
 
-/*
-close modal 
-*/
-const closeModal = () => {
-emit('update:modelValue', false)
-}
-
-
-const modalCardRef = ref(null) 
-
-onClickOutside(modalCardRef, closeModal)
+  /*
+  close modal 
+  */
+  const closeModal = () => {
+  emit('update:modelValue', false)
+  }
 
 
-</script>
+  const modalCardRef = ref(null) 
 
-<template>
-<div class="modal is-active p-2">
-  <div class="modal-background"></div>
-  <div 
-  class="modal-card"
-  ref="modalCardRef">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Delete note?</p>
-      <button class="delete" aria-label="close" @click="closeModal"></button>
-    </header>
-    <section class="modal-card-body">
-      Are you sure you want to delete this note? 
-    </section>
-    <footer class="modal-card-foot is-justify-content-flex-end">
-    <button
-    class="button"
-    @click="closeModal">
-    Cancel
-    </button>
-      <button class="button is-danger">Delete note</button>
-    </footer>
+  onClickOutside(modalCardRef, closeModal)
+
+  /*
+  keyboard control
+  */
+
+  const handleKeyboard = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      closeModal()
+      }
+  }
+  onMounted(() => {
+    document.addEventListener("keyup", handleKeyboard)
+  })
+
+  onUnmounted(() => {
+    document.removeEventListener("keyup", handleKeyboard)
+  })
+  </script>
+
+  <template>
+  <div class="modal is-active p-2">
+    <div class="modal-background"></div>
+    <div 
+    class="modal-card"
+    ref="modalCardRef">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Delete note?</p>
+        <button class="delete" aria-label="close" @click="closeModal"></button>
+      </header>
+      <section class="modal-card-body">
+        Are you sure you want to delete this note? 
+      </section>
+      <footer class="modal-card-foot is-justify-content-flex-end">
+      <button
+      class="button"
+      @click="closeModal">
+      Cancel
+      </button>
+        <button class="button is-danger">Delete note</button>
+      </footer>
+    </div>
   </div>
-</div>
-</template>
+  </template>
