f2db873289332add5af7a87e68e186bc39733773
Malin Nilsson <malinnilsson87@gmail.com>
2023-03-11 11:12:09 +0100

    Login & Register page (tabs)

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,61 +1,76 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 import { onClickOutside } from '@vueuse/core'
 /*
 mobile nav 
 */
 const showMobileNav = ref(false)
-const navbarMenuRef = ref(null) 
+const navbarMenuRef = ref(null)
 const navbarBurgerRef = ref(null)
 
-onClickOutside(navbarMenuRef, (event) => {
-  showMobileNav.value = false
-}, {
-  ignore: [navbarBurgerRef]
-})
-
+onClickOutside(
+  navbarMenuRef,
+  (event) => {
+    showMobileNav.value = false
+  },
+  {
+    ignore: [navbarBurgerRef],
+  }
+)
 </script>
 
 <template>
- <nav 
- class="navbar is-light" 
- role="navigation" 
- aria-label="main navigation">
-  <div class="container is-max-desktop px-2"> 
-  <div class="navbar-brand">
-    <div 
-    class="navbar-item is-size-4 is-family-sans-serif">
-     Noteable
-    </div>
+  <nav
+    class="navbar is-link-light"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="container is-max-desktop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-sans-serif">Noteable</div>
 
-    <a 
-    @click.prevent="showMobileNav = !showMobileNav"
-    role="button" 
-    ref="navbarBurgerRef"
-    class="navbar-burger"
-    :class="{'is-active' : showMobileNav}" 
-    aria-label="menu" 
-    aria-expanded="false" 
-    data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          role="button"
+          ref="navbarBurgerRef"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-  <div 
-  id="navbarBasicExample" 
-  class="navbar-menu"
-  :class="{'is-active' : showMobileNav}"
-  ref="navbarMenuRef" >
-    <div class="navbar-end">
-      <RouterLink to="/" class="navbar-item" active-class="is-active" @click="showMobileNav = false">Notes</RouterLink>
-      <RouterLink to="/stats" class="navbar-item" active-class="is-active" @click="showMobileNav = false">Stats</RouterLink>
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileNav }"
+        ref="navbarMenuRef"
+      >
+        <div class="navbar-end">
+          <RouterLink
+            to="/"
+            class="navbar-item"
+            active-class="is-active"
+            @click="showMobileNav = false"
+            >Notes</RouterLink
+          >
+          <RouterLink
+            to="/stats"
+            class="navbar-item"
+            active-class="is-active"
+            @click="showMobileNav = false"
+            >Stats</RouterLink
+          >
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</nav>
+  </nav>
 </template>
 
 <style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
 import ViewEditNote from "@/views/ViewEditNote.vue";
+import ViewAuth from "@/views/ViewAuth.vue";
+
 
 const routes = [
   {
@@ -18,6 +20,11 @@ const routes = [
     path: "/stats",
     name: "stats",
     component: ViewStats,
+  },
+  {
+    path: "/auth",
+    name: "auth",
+    component: ViewAuth,
   },
 ];
 
